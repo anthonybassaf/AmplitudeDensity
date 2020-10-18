1edc618b85ae5025c3c390fdd1b41780c89806c7
anthonybassaf <60676144+anthonybassaf@users.noreply.github.com>
2020-10-18 11:30:53 -0400

    plotting amplitudes for different bins = 10, 50, 100

---
+++ amplitudedensity.py
@@ -0,0 +1,37 @@
+import csv
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn')
+
+def plotData():
+    data = open('file1.csv', newline='')
+    reader = csv.reader(data)
+    values = list(reader)
+    nums = list()
+
+    for value in values:
+        nums.append(float(value[0]))
+
+ 
+    
+    fig, (ax1, ax2, ax3) = plt.subplots(ncols = 3, sharex = True, sharey = True)
+    ax1.hist(nums, 10, ec = "black")
+    ax2.hist(nums, 50, ec = "black")
+    ax3.hist(nums, 100, ec = "black")
+
+    fig.suptitle("Amplitude for Bins  = 10, 50, 100")
+
+    ax1.set_xlabel("Range")
+    ax1.set_ylabel("Amplitude")
+
+    ax2.set_xlabel("Range")
+    ax2.set_ylabel("Amplitude")
+
+    ax3.set_xlabel("Range")
+    ax3.set_ylabel("Amplitude")
+
+    plt.show()
+
+plotData()
